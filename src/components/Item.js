2b9e957ed96ed471ca5cac208f94e38f67f1cbf2
Philip Samoei <samphilkerich04@gmail.com>
2023-04-23 17:35:58 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function Item({ name, category }) {
   return (
-    <li className="">
+    <li className="in-cart">
+      {/* the item is in the cart */}
+      <li className="">
+        {/* the item is NOT in the cart */}
+      </li>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button className="add">Add to Cart</button>
